60dd262df45411bf3c47462080f6099444704fdb
Jo40519 <jaolobato85@gmail.com>
2023-04-23 12:19:07 -0300

    aa

---
+++ estacao-back/estacao-back/src/main/java/com/estudosspring/estacaoback/EstacaoBackApplication.java
@@ -0,0 +1,13 @@
+package com.estudosspring.estacaoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacaoBackApplication.class, args);
+	}
+
+}
